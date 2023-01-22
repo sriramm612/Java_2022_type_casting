f001e000bf92b9fe415d67d3ec4d43be5ffd1f83
sriramm612 <sriramm612@gmail.com>
2023-01-22 10:40:16 +0530

    done

---
+++ Downcast_3102022.java
@@ -0,0 +1,13 @@
+package type_casting.com;
+
+public class Downcast_3102022 {
+
+	public void plane() {
+		// TODO Auto-generated method stub
+    System.out.println("Boeing");
+	}
+   public void rail() {
+	// TODO Auto-generated method stub
+   System.out.println("Train");
+}
+}
