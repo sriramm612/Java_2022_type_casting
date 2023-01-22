1a1c149c1e7ee932134bb5f2d27302a6dd12b294
sriramm <sriramm612@gmail.com>
2023-01-22 15:28:14 +0530

    yes

---
+++ Upcasting_child.java
@@ -0,0 +1,20 @@
+package type_casting.com;
+
+public class Upcasting_child extends Upcasting_3102022 {
+
+	@Override
+	public void bike() {
+		// TODO Auto-generated method stub
+		System.out.println("Honda hness");
+	}
+	@Override
+	public void car() {
+		// TODO Auto-generated method stub
+		System.out.println("Ford figo");
+	}
+	@Override
+	public void suv() {
+		// TODO Auto-generated method stub
+		System.out.println("Ford ecosport");
+	}
+}
